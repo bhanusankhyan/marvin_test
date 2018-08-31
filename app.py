8ceb5b4797d1ade1e5093ede1b84fedd91b283cc
bhanusankhyan <bhanusankhyan77@gmail.com>
2018-08-31 16:09:47 +0530

    Tabler

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from database_setup import Users, Base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite:////home/bhanu/Documents/final_build/flask2/marvin1.db')
-Base.metadata(bind = engine)
+engine = create_engine('sqlite:////home/bhanu/Documents/final_build/flask4/marvin1.db')
+Base.metadata.bind = engine
 
 app = Flask(__name__)
 
@@ -12,8 +12,21 @@ DBsession = sessionmaker(bind=engine)
 session = DBsession()
 DBsession.autoflush = True
 
+
+@app.context_processor
+def Dashboard_details():
+    navbar_items = {'Home':'/','Components':[{'name':'title1','link':'#'},{'name':'title2','link':'#'}], 'Dropdown':[{'name':'title1', 'link':'#'},{'name':'title2','link':'#'}]}
+    user_menu_items = [{'name':'Profile','link':'#','class':'fe fe-user'},{'name':'Settings','link':'#','class':'fe-settings'},
+    {'name':'Inbox','link':'#','class':'fe fe-mail'},{'name':'Message','link':'#','class':'fe-send'},
+    {'name':'Need Help?','link':'#','class':'fe-help-circle'},{'name':'SignOut','link':'#','class':'fe-log-out'}]
+    return dict(navbar_items = navbar_items,user_menu_items = user_menu_items,type = type, str = str)
+
+
+
 @app.route('/')
-def index():
-    return render_template('dashboard.html')
+def home():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run(port= 8000, host ='0.0.0.0', debug = True)
+    app.run(host = '0.0.0.0',port= 8000, debug = True)
